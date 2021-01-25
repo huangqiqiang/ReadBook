54eefc3e168e6f39c1ee58b047010eca085f30ac
593979591@qq.com <zz789789>
2021-01-25 20:24:58 +0800

    保存下代码

--- app/src/androidTest/java/com/qq/readbook/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/qq/readbook/ExampleInstrumentedTest.kt
@@ -2,7 +2,9 @@ package com.qq.readbook
 
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.platform.app.InstrumentationRegistry
+import com.qq.readbook.repository.SearchBookRepository
 import com.qq.readbook.repository.read.JsoupUtils
+import org.jsoup.Jsoup
 import org.junit.Test
 import org.junit.runner.RunWith
 import java.io.InputStream
@@ -23,12 +25,12 @@ class ExampleInstrumentedTest {
         val buffer = ByteArray(lenght)
         inputStream.read(buffer)
         val result = String(buffer)
-        val json= JsoupUtils.getJsonElement("{\"ruleSearchList\":[{\"elementType\":\"class\",\"elementValue\":\"library\",\"ruleChild\":{\"elementType\":\"tag\",\"elementValue\":\"li\"}}],\"ruleBookName\":[{\"elementType\":\"class\",\"elementValue\":\"bookname\",\"attrValue\":\"text\"}],\"ruleDesc\":[{\"elementType\":\"class\",\"elementValue\":\"intro\",\"attrValue\":\"text\"}],\"ruleAuthor\":[{\"elementType\":\"class\",\"elementValue\":\"author\",\"attrValue\":\"text\"}],\"newestChapterTitle\":[{\"elementType\":\"class\",\"elementValue\":\"chapter\",\"attrValue\":\"text\"}],\"chapterUrl\":[{\"elementType\":\"class\",\"elementValue\":\"bookimg\",\"attrValue\":\"href\",\"formatRule\":{\"type\":\"replaceEnd\",\"value\":\".html\",\"formatRule\":{\"type\":\"addStart\",\"value\":\"https://www.dstiejuan.com\"}}}],\"ruleImg\":[{\"elementType\":\"tag\",\"elementValue\":\"img\",\"attrValue\":\"src\",\"position\":0}],\"ruleType\":{\"elementType\":\"tag\",\"elementValue\":\"p\",\"ruleChild\":{\"elementType\":\"tag\",\"elementValue\":\"a\",\"position\":1}}}")
-//        SearchBookRepository.doReadBookList4Source(json,result)
+        val doc = Jsoup.parse(result)
+        val json =JsoupUtils.getJsonElement("{\"ruleSearchList\":[{\"elementType\":\"class\",\"elementValue\":\"library\",\"ruleChild\":{\"elementType\":\"tag\",\"elementValue\":\"li\"}}],\"ruleBookName\":[{\"elementType\":\"class\",\"elementValue\":\"bookname\",\"attrValue\":\"text\"}],\"ruleDesc\":[{\"elementType\":\"class\",\"elementValue\":\"intro\",\"attrValue\":\"text\"}],\"ruleAuthor\":[{\"elementType\":\"class\",\"elementValue\":\"author\",\"attrValue\":\"text\"}],\"newestChapterTitle\":[{\"elementType\":\"class\",\"elementValue\":\"chapter\",\"attrValue\":\"text\"}],\"chapterUrl\":[{\"elementType\":\"class\",\"elementValue\":\"bookimg\",\"attrValue\":\"href\",\"formatRule\":{\"type\":\"replaceEnd\",\"value\":\".html\",\"formatRule\":{\"type\":\"addStart\",\"value\":\"https://www.dstiejuan.com\"}}}],\"ruleImg\":[{\"elementType\":\"tag\",\"elementValue\":\"img\",\"attrValue\":\"src\",\"position\":0}],\"ruleType\":{\"elementType\":\"tag\",\"elementValue\":\"p\",\"ruleChild\":{\"elementType\":\"tag\",\"elementValue\":\"a\",\"position\":1}}}")
+
+//        SearchBookRepository.doReadBookList4Source(json, result)
 
     }
-
-
 
 
 }
